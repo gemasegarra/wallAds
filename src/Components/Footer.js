e32ab9f46d52b18da8e9988c47117324ed995de8
gemasegarra <gema.segb@gmail.com>
2020-02-14 22:20:56 +0100

    create footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const FooterBar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  bottom: 0;
+  width: 100%;
+  height: 85px;
+  background: #F6F6F6;
+  font-size: 0.7rem;
+  a {
+    color: #db7093;
+    text-decoration: none;
+  }
+  & a:visited {
+    color: #db7093;
+  }
+  `
+
+class Footer extends Component {
+  render() {
+    return (
+      <FooterBar>
+        <p>Coded by <a href='https://github.com/gemasegarra'> Gema S.</a> | 2020</p>
+      </FooterBar>
+     )
+  }
+};
+
+export default Footer;
