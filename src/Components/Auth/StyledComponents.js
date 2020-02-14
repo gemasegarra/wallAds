d4e9deb969338483b5add9df3b5aed36bb027599
gemasegarra <gema.segb@gmail.com>
2020-02-15 00:04:07 +0100

    add style to Auth components

---
+++ src/Components/Auth/StyledComponents.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const Login = styled.h1 `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+`;
+
+export const FormContainer = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`; 
+
+export const Form = styled.form `
+  border: 1px solid #db7093; 
+  border-radius: 3px;
+  padding: 1.2rem; 
+  margin-bottom: 1rem;
+`;  
+
+export const InputField = styled.div `
+  margin: 1rem;
+  label {
+    margin-right:1rem;
+  }
+  input {
+    border: none;
+    border-bottom: 1px solid #db7093;
+    outline: none;
+    -webkit-appearance:none;
+  }
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  color: #db7093;
+  font-size: 1rem;
+  margin: auto;
+  padding: 0.25rem 1rem;
+  border: 1px solid #db7093;
+  border-radius: 3px;
+  &:hover {
+    background-color: #F6F6F6;
+  }
+`;
+
+export const CreateAccount = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
+`
+export const StyledLink = styled.a `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
+`
