f3bf9e94760dc98dd870f1880a477ba34be0a60b
gemasegarra <gema.segb@gmail.com>
2020-02-15 16:57:48 +0100

    update handleChange and handleSubmit functions

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -10,30 +10,34 @@ class SignIn extends Component {
   }
 
   handleChange = (e) => {
-    console.log(e)
+    this.setState({
+      [e.target.id]: e.target.value
+    })
   }
   handleSubmit = (e) => {
-    console.log(e)
+    e.preventDefault();
+    console.log(this.state)
   }
+
   render() {
     return (
       <>
-      <Login>Sign in to NodePop</Login>
-      <FormContainer>
-      <Form onSubmit={this.handleSubmit}>
-        <InputField>
-          <label htmlFor='username'>Username:</label> 
-          <input type='text' id='username' onChange={this.handleSubmit}></input>
-        </InputField>
-        <InputField>
-          <label htmlFor='password'>Password: </label> 
-          <input type='password' id='password'  onChange={this.handleChange}></input>
-        </InputField>
-        <Button>Sign in</Button>
-      </Form> 
-      </FormContainer>
-      <CreateAccount>New to NodePop?</CreateAccount>
-      <StyledLink><Link to='/SignUp'> Register here</Link>.</StyledLink>
+        <Login>Sign in to NodePop</Login>
+        <FormContainer>
+          <Form onSubmit={this.handleSubmit}>
+            <InputField>
+              <label htmlFor='username'>Username:</label>
+              <input type='text' id='username' onChange={this.handleChange}></input>
+            </InputField>
+            <InputField>
+              <label htmlFor='password'>Password: </label>
+              <input type='password' id='password' onChange={this.handleChange}></input>
+            </InputField>
+            <Button>Sign in</Button>
+          </Form>
+        </FormContainer>
+        <CreateAccount>New to NodePop?</CreateAccount>
+        <StyledLink><Link to='/SignUp'> Register here</Link>.</StyledLink>
       </>
     )
   }
--- src/Components/Auth/SignUp.js
+++ src/Components/Auth/SignUp.js
@@ -6,36 +6,39 @@ import { Login, FormContainer, Form, InputField, Button, CreateAccount, StyledLi
 
 class SignUp extends Component {
   state = {
-    user: '',
+    username: '',
     password: '',
-  }  
+  }
 
   handleChange = (e) => {
-    console.log(e)
+    this.setState({
+      [e.target.id]: e.target.value
+    })
   }
   handleSubmit = (e) => {
-    console.log(e)
+    e.preventDefault();
+    console.log(this.state)
   }
 
   render() {
     return (
-          <>
-      <Login>Sign up for NodePop</Login>
-      <FormContainer>
-      <Form onSubmit={this.handleSubmit}>
-        <InputField>
-          <label htmlFor='username'>Username:</label> 
-          <input type='text' id='username' onChange={this.handleSubmit}></input>
-        </InputField>
-        <InputField>
-          <label htmlFor='password'>Password: </label> 
-          <input type='password' id='password'  onChange={this.handleChange}></input>
-        </InputField>
-        <Button>Sign up</Button>
-      </Form> 
-      </FormContainer>
-      <CreateAccount>Already have an account?</CreateAccount>
-      <StyledLink><Link to='/SignIn'> Sign in here</Link>.</StyledLink>
+      <>
+        <Login>Sign up for NodePop</Login>
+        <FormContainer>
+          <Form onSubmit={this.handleSubmit}>
+            <InputField>
+              <label htmlFor='username'>Username:</label>
+              <input type='text' id='username' onChange={this.handleChange}></input>
+            </InputField>
+            <InputField>
+              <label htmlFor='password'>Password: </label>
+              <input type='password' id='password' onChange={this.handleChange}></input>
+            </InputField>
+            <Button>Sign up</Button>
+          </Form>
+        </FormContainer>
+        <CreateAccount>Already have an account?</CreateAccount>
+        <StyledLink><Link to='/SignIn'> Sign in here</Link>.</StyledLink>
       </>
     )
   }
