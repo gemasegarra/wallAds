94cb352fd421fafa04fc6f2f3e8fe9d2745bfa8e
gemasegarra <gema.segb@gmail.com>
2020-02-15 20:30:11 +0100

    create AdDetail component

---
+++ src/Components/Ads/AdDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AdDetail extends Component {
+  render () {
+    return (
+      <div>Ad details here </div>
+    )
+  }
+};
+
+export default AdDetail;
