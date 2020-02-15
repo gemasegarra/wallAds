b650e1afd1f95c8a3637e5b8df05d3db7cd7d54b
gemasegarra <gema.segb@gmail.com>
2020-02-15 20:30:27 +0100

    add AdList & AdDetail routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import SignIn from './Components/Auth/SignIn';
 import SignUp from './Components/Auth/SignUp';
-import Footer from './Components/Footer';
+import AdList from './Components/Ads/AdList';
+import AdDetail from './Components/Ads/AdDetail';
+import Footer from './Components/Layout/Footer';
 
 import { createGlobalStyle } from 'styled-components';
 
@@ -24,6 +26,8 @@ function App() {
           <Route path='/' exact component={SignUp} />
           <Route path='/SignIn' component={SignIn} />
           <Route path='/SignUp' component={SignUp} />
+          <Route path='/AdList' component={AdList} />
+          <Route path='/AdDetail/:id' component={AdDetail} />
         </Switch>
       </Router>
       <Footer></Footer>
