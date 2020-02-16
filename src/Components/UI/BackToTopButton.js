2bd5e15a177a198552f0138d93d46892703dea1f
gemasegarra <gema.segb@gmail.com>
2020-02-16 16:26:01 +0100

    create BackToTopButton

---
+++ src/Components/UI/BackToTopButton.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { ArrowAltCircleUp } from 'styled-icons/fa-regular/ArrowAltCircleUp';
+
+const Arrow = styled(ArrowAltCircleUp)`
+  position: fixed;
+  cursor: pointer;
+  bottom: 90px;
+  right: 0;
+  color: grey;
+  width: 90px;
+  height: 30px;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  &:hover {
+    color: #8e3b56;
+  }
+`
+
+class GoUpButton extends Component {
+	state = {
+		hasScrolled: false
+	};
+
+	componentDidMount() {
+		document.addEventListener('scroll', this.onScroll)
+	};
+	onScroll = () => {
+			if (window.scrollY > 100) {
+				this.setState({ hasScrolled: true })
+			} else {
+				this.setState({ hasScrolled: false })
+			}
+		};
+	scrollToTop = () => {
+		window.scrollTo(0, 0);
+	}
+	goUp = () => {
+		if (this.state.hasScrolled) {
+			return (
+				<Arrow onClick={this.scrollToTop}>
+				</Arrow>
+			)
+		}
+	}
+	render() {
+		return (
+			<>
+				{this.goUp()}
+			</>
+		)
+	}
+};
+
+export default GoUpButton;
