41290fc21df271469999ac3753f4b551ab4e9a26
gemasegarra <gema.segb@gmail.com>
2020-02-17 13:53:01 +0100

    navbar

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
-import { HeaderBar } from './StyledComponents'
+import { HeaderBar, StyledLink, StyledNavLink } from './StyledComponents'
+
 
 class Header extends Component {
   render() {
     return (
       <HeaderBar>
-        <h1>NodePop!</h1> 
+        <StyledLink exact to='/adlist'>NodePop!</StyledLink>
         <p>Buy and sell anything you want</p>
+        <StyledNavLink exact to="/adlist" activeClassName="active">
+          Ad List
+        </StyledNavLink>
+        <StyledNavLink exact to="/createad" activeClassName="active">
+          Create ad
+        </StyledNavLink>
       </HeaderBar>
     )
   }
