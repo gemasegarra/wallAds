58db600ddbf2573ba4f0c7a9015d7e8e019496c9
gemasegarra <gema.segb@gmail.com>
2020-02-18 20:01:16 +0100

    create AdDetail component

--- src/Components/Ads/AdDetail.js
+++ src/Components/Ads/AdDetail.js
@@ -1,11 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from 'axios';
+import { AdCard, AdContent, CardTitle, Price, Picture, Button, StyledLink } from './StyledComponents';
+import Header from '../Layout/Header';
+import GoUpButton from '../UI/BackToTopButton';
 
-class AdDetail extends Component {
-  render () {
-    return (
-      <div>Ad details here </div>
-    )
+
+class AdDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      price: Number,
+      description: '',
+      tags: [],
+      type: '',
+      photo: ''
+    };
   }
-};
+
+  componentDidMount() {
+    const { match: { params } } = this.props;
+
+    axios.get(`http://34.89.93.186:8080/apiv1/anuncios/${params.id}`, {
+      withCredentials: true,
+    })
+      .then(res => {
+
+        console.log(res.data.result);
+        this.setState(
+          res.data.result
+        )
+        console.log(this.state)
+      })
+  }
+
+  render() {
+    const { name, price, description, tags, type, photo, id } = this.state;
+
+    return (
+      <> 
+      <Header></Header>
+      <AdCard key={id}>
+        <AdContent>
+          <CardTitle>{name}</CardTitle>
+          <div>Want to {type}</div>
+          <Price>Price: {price}€</Price>
+          <p>{description}</p>
+          <p>Tags: {tags}</p>
+          <Picture alt='product photo' src={photo}></Picture>
+          <StyledLink to='/adlist'>
+<Button>Go back to ad list</Button></StyledLink>
+        </AdContent>
+      </AdCard>
+      <GoUpButton></GoUpButton></>
+    );
+  }
+}
+
 
 export default AdDetail;
