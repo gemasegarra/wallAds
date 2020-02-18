968a35608efe559c005ea6de4a92ec0633f09679
gemasegarra <gema.segb@gmail.com>
2020-02-18 20:05:12 +0100

    style link

--- src/Components/Ads/StyledComponents.js
+++ src/Components/Ads/StyledComponents.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const AdCard = styled.div`
-  margin: 1rem 6rem;
-  width: 70%;
+  margin: 1rem 5rem;   
 `
 
 export const AdContent = styled.div`
   border: 1px solid #8e3b56;
   padding: 1rem;
-  background: #F6F6F6		
+  background: #F6F6F6;
 `
 
 export const CardTitle = styled.h1`
-  font-size: 1.7rem;
-  color: #427e8e;
+  font-size: 1.8rem;
+  color: #8e3b56;
 `
 
 export const Description = styled.p`
@@ -46,4 +46,11 @@ export const Button = styled.button`
 
 export const NoAds = styled.div`
   margin: 1rem 6rem;
+`
+export const StyledLink = styled(Link) `
+margin-right: auto;
+margin-left: 6rem;
+font-size: 2.5rem;
+text-decoration: none;
+color: #db7093;
 `
