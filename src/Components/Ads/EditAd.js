63b2c5d692c47f6eabc7ef01cd4570f8a184c591
gemasegarra <gema.segb@gmail.com>
2020-02-19 11:21:01 +0100

    create EdtAd component

---
+++ src/Components/Ads/EditAd.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class EditAd extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  
+  render() {
+
+    return (
+
+      <>
+      </>
+    )
+
+  }
+}
+
+export default EditAd;
